f1dd647860cd661bc14a6bd245107c885f0e81c2
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 14:42:12 +0800

    ch14: [feat] getting the rate from hashtable

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -14,9 +14,8 @@ public class Bank {
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD"))
-                ? 2
-                : 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
     }
 
     private class Pair {
