53f1e92f52752fe58b828e436b5971e4ef1293c8
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 14:24:06 +0800

    ch13: [refactor] remove useless if condition by polymorphism as Expression

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -2,9 +2,6 @@ package money;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        if (source instanceof Money)
-            return (Money) source.reduce(to);
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }
