b0c4a4e12667d83d9345a3e99cc9ce1c4f60abfd
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 13:00:19 +0800

    ch8: [refactor] adjust test case from Dollar constructor to Money.dollar() factory method

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -8,17 +8,17 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     public void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        assertFalse(new Franc(5).equals(Money.dollar(5)));
     }
 
     @Test
