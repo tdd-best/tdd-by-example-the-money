35d64fd24100251437e25b94c933aec42a9ae582
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 11:47:40 +0800

    ch5: [feat] complete Franc class by copying from Dollar class

---
+++ src/main/java/money/Franc.java
@@ -0,0 +1,18 @@
+package money;
+
+class Franc {
+    private int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+}
