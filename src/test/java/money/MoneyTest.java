f7c59e1050dbe585c89baad284f449ff604ef26c
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 12:49:19 +0800

    ch7: [experiment] equality compare with Franc and Dollar?

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -18,6 +18,7 @@ public class MoneyTest {
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 
     @Test
