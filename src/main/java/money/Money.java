69f762b99f1a48edabc96013a201afcf32055408
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 13:45:24 +0800

    ch11: [refactor] return Money instead of Franc or Dollar instance in dollar() and franc() factory methods, to remove reference of Dollar and Franc constructor

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -12,11 +12,11 @@ public class Money {
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     public boolean equals(Object object) {
