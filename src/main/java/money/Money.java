122bde68e7ae4cd660dc62710c13bf41dd1c5217
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 13:34:41 +0800

    ch10: [fix] equality comparing by amount and currency, instead of type name

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -22,7 +22,7 @@ public class Money {
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals(money.currency());
     }
 
     @Override
