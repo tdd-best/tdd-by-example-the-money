2c332cef0cac27fea43a6bf8d5a8b2013e07e230
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 13:50:27 +0800

    ch12: [test] add test simple addition for 5 dollar + 5 dollar should be 10 dollar

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -31,4 +31,10 @@ public class MoneyTest {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
     }
+
+    @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
 }
