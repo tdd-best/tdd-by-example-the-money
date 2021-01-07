be83b1c6b39ce4da305afd187307531f4c4a0171
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 13:17:30 +0800

    ch9: [refactor] use factory method, instead of Franc constructor to make currency setting via factory method

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -8,6 +8,6 @@ class Franc extends Money {
     }
 
     Money times(int multiplier) {
-        return new Franc(amount * multiplier, null);
+        return Money.franc(amount * multiplier);
     }
 }
