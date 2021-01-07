61a3ab84910c389d960f07a7c76a5e18401ece07
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 12:55:35 +0800

    ch8: [feat] use factory method to return Dollar instance

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -3,6 +3,10 @@ package money;
 public class Money {
     protected int amount;
 
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
