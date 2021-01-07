faf542e8df31634c573968d688e25dbe3e6def47
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 14:40:17 +0800

    ch14: [feat] create a Hashtable field "rates" for setting rates

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -1,6 +1,10 @@
 package money;
 
+import java.util.Hashtable;
+
 public class Bank {
+    private Hashtable rates = new Hashtable();
+
     public Money reduce(Expression source, String to) {
         return source.reduce(this, to);
     }
