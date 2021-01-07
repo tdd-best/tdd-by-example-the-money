2cb372430300d7ec7479557b7632051020d28b48
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 15:00:21 +0800

    ch15: [test] add test "5 dollars + 10 francs should be 10 dollars"

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -72,4 +72,14 @@ public class MoneyTest {
         Money result = bank.reduce(Money.franc(2), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testMixedAddition() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
 }
