d056716b969f4fd495a5dce0d968426536db314f
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 12:50:52 +0800

    ch7: [fix] equals() not only compare with amount, but also type

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -2,8 +2,10 @@ package money;
 
 public class Money {
     protected int amount;
+
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount
+                && getClass().equals(money.getClass());
     }
 }
