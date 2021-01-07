595e8416c5bcb43f822f628b2a23ec322f637b56
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 11:12:37 +0800

    ch2: [compile successfully] change times() API to return Dollar, and hard-code return null to compile successfully

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -7,7 +7,8 @@ public class Dollar {
         this.amount = amount;
     }
 
-    public void times(int multiplier) {
+    public Dollar times(int multiplier) {
         amount *= multiplier;
+        return null;
     }
 }
