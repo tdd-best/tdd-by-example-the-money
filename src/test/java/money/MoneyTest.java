f90bc37a52ade13d410ca47af34f9accfd9de1a6
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 15:23:26 +0800

    ch16: [experiment] remove experiment test case

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -104,10 +104,4 @@ public class MoneyTest {
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(20), result);
     }
-
-    @Test
-    public void testPlusSameCurrencyReturnsMoney() {
-        Expression sum = Money.dollar(1).plus(Money.dollar(1));
-        assertTrue(sum instanceof Money);
-    }
 }
