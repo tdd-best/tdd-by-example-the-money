9407c7d0255134438cb53736cdfaa5b608b58853
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 12:53:14 +0800

    ch8: [refactor] make Dollar's times() and Franc's times() return the same type Money, to show them duplicated in abstract level

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -6,7 +6,7 @@ class Franc extends Money {
         this.amount = amount;
     }
 
-    Franc times(int multiplier) {
+    Money times(int multiplier) {
         return new Franc(amount * multiplier);
     }
 }
