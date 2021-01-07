21832150278830f72a052b5411a17da45769aff7
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 15:18:08 +0800

    ch16: [test] add test for Sum.times() feature

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -93,4 +93,15 @@ public class MoneyTest {
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(15), result);
     }
+
+    @Test
+    public void testSumTimes() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(20), result);
+    }
 }
