baa75879b8c36694f513a54652cd1f1652bdc7cf
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 11:07:59 +0800

    ch2: [experiment] try five.times(3) should be 15 by current API

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -10,5 +10,7 @@ public class MoneyTest {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
 }
