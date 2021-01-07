139ad73c45d1c1c58df15cd880dee4c4c3baef2f
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 13:47:34 +0800

    ch11: [refactor] remove duplicate meaning assertions

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -16,8 +16,6 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
