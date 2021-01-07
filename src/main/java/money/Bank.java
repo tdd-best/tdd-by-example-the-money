ff51699104ef6dfa738b89feff6b828cebe45b24
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 14:39:25 +0800

    ch14: [feat] override Pair.equals() and hashCode()

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -23,5 +23,14 @@ public class Bank {
             this.from = from;
             this.to = to;
         }
+
+        public boolean equals(Object object) {
+            Pair pair = (Pair) object;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        public int hashCode() {
+            return 0;
+        }
     }
 }
