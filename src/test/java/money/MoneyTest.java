7d067ecac04c316a7eb864034c8cf1ef7fc6ff54
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 12:56:45 +0800

    ch8: [test] make five dollar variable as Money to remove reference of Dollar class

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoneyTest {
     @Test
     public void testMultiplication() {
-        Dollar five = Money.dollar(5);
+        Money five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
