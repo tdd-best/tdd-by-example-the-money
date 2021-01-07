4caabd84bf9ee694374a5e1b878cd89751ce2f71
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 14:20:13 +0800

    ch13: [test] add test when bank.reduce() pass a money as Expression parameter. And hard-code if condition only for Money type

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -2,6 +2,7 @@ package money;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
+        if (source instanceof Money) return (Money) source;
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -57,4 +57,11 @@ public class MoneyTest {
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(7), result);
     }
+
+    @Test
+    public void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
