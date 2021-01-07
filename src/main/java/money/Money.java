1ad23eb49c19155cd2655e4a4c80a05b18069110
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 15:08:08 +0800

    ch15: [refactor] change Money.times() return type from Money to Expression

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -41,7 +41,7 @@ public class Money implements Expression {
         return new Sum(this, addend);
     }
 
-    Money times(int multiplier) {
+    Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 }
