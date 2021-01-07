7ab6d23f40fc647750c362c3029ac7afd1255329
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 14:13:10 +0800

    ch13: [test] add test reduce sum when currency is the same

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -49,4 +49,12 @@ public class MoneyTest {
         assertEquals(five, sum.augend);
         assertEquals(five, sum.addend);
     }
+
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
 }
