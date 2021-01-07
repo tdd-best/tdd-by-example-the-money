b4dd8a8aba94920fa112e5d5c8bbbebf31d8fa6f
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 13:05:36 +0800

    ch9: [test] add test for currency() with dollar and franc money

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -27,4 +27,10 @@ public class MoneyTest {
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
     }
+
+    @Test
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
 }
