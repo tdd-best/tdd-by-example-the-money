2b463a1f5d24e383bb9c75d5677071362f21cabd
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 12:24:52 +0800

    ch6: [refactor] remove Franc's equals() because of duplication with Super class Money's equals()

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -6,11 +6,6 @@ class Franc extends Money {
         this.amount = amount;
     }
 
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount;
-    }
-
     Franc times(int multiplier) {
         return new Franc(amount * multiplier);
     }
