784b081d4dae64797b3de1d3b776d20181105d7c
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 11:45:35 +0800

    ch5: [test] add France multiplication test case

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -17,4 +17,11 @@ public class MoneyTest {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
+
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
 }
