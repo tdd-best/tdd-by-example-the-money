9a40105cb3c040688fdc4c174b33552c722c543a
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 11:26:08 +0800

    ch4: [test] refactor test to assertEquals() with Dollar instance, instead of amount value in test

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -9,7 +9,7 @@ public class MoneyTest {
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), product); // Dollar 跟 Dollar 比較
         product = five.times(3);
         assertEquals(15, product.amount);
     }
