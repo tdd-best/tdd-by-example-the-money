42789876e8bdd27acb45f0aa1c6ca06e1757f2ab
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 14:14:36 +0800

    ch13: [feat] complete reduce() by plus amount from augend and addend of sum. It works when the same currency.

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -2,6 +2,8 @@ package money;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        Sum sum = (Sum) source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, to);
     }
 }
