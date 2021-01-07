fa9a111f645c5db94cd92ca1502897b92bafba78
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 14:06:56 +0800

    ch13: [experiment] add test for plus() operation should return a Expression result as Sum with augend and addend

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -40,4 +40,13 @@ public class MoneyTest {
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), reduced);
     }
+
+    @Test
+    public void testPlusReturnsSum() {
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five, sum.augend);
+        assertEquals(five, sum.addend);
+    }
 }
