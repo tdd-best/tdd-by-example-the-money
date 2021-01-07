58ca9b780d11c229fa53b5b2fd02eaea1b13c68b
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 14:28:04 +0800

    ch14: [test] add test reduce money when different currency

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -64,4 +64,12 @@ public class MoneyTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
