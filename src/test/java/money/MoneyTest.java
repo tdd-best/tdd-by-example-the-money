103846a0c4fa979ee23d440ef18f7c76541a92b1
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 11:28:11 +0800

    ch4: [refactor] inline the temporary variable "product"

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -8,10 +8,8 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(new Dollar(10), product);
-        product = five.times(3);
-        assertEquals(new Dollar(15), product); // 也改用 Dollar 跟 Dollar 比
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
