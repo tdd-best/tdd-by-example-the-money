885889afee7cad60c1b418022542e359b694b84c
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 11:09:58 +0800

    ch2: [test] define how we want to use times(), it should return another Dollar object

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -8,9 +8,9 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
-        five.times(3);
-        assertEquals(15, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
