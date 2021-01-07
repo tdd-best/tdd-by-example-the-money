21852b4d46c005cfafc88034732755b2439ef9a5
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 11:22:09 +0800

    ch3: [test] add the second assertion for Triangulation to make production code work in real

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -2,8 +2,7 @@ package money;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
@@ -18,5 +17,6 @@ public class MoneyTest {
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
