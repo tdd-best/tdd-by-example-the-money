71531906fd85bae6a0688b51c77a2c4499711394
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 12:18:13 +0800

    ch6: [refactor] make object convert type from (Dollar) to (Money)

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -8,7 +8,7 @@ public class Dollar extends Money {
 
     @Override
     public boolean equals(Object object) {
-        Money dollar = (Dollar) object;
+        Money dollar = (Money) object;
         return amount == dollar.amount;
     }
 
