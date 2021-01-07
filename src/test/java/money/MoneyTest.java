e09b651a98324473645163e8ef3347f4a7d35ff2
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 12:20:52 +0800

    ch6: [test] add Franc equality test cases

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -16,6 +16,8 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 
     @Test
