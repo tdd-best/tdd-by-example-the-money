ad53bf75451b64cd1fcefe5e82a0174efd0eb76d
Joey Chen <jay.hatelove@gmail.com>
2021-01-07 11:14:14 +0800

    ch2: [refactor] return new Dollar instance in times()

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -8,7 +8,6 @@ public class Dollar {
     }
 
     public Dollar times(int multiplier) {
-        amount *= multiplier;
-        return null;
+        return new Dollar(amount * multiplier);
     }
 }
